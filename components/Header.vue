4b60bab949ebf7d149048f7ef9fcfcc11b4da9f4
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2023-08-28 15:45:24 +0200

    Add WP queries

---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="relative w-full p-8 h-100px mx-auto bg-zinc-100">
+    <h1 class="text-4xl" >Nuxt WP</h1>
+  </header>
+</template>
---
+++ pages/[...uri].vue
@@ -0,0 +1,4 @@
+<template>
+  <h1 class="text-2xl">Here a catch all page</h1>
+  <p>Catch all page is useful to match complex uri used in WordPress</p>
+</template>
